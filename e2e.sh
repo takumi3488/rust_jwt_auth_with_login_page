679c94dfe91e9990fa6467e614d57053bcd60e38
Takumi Mori <51111242+takumi3488@users.noreply.github.com>
2024-08-20 21:11:01 +0900

    add: e2e

--- e2e.sh
+++ e2e.sh
@@ -1,26 +1,27 @@
 #!/bin/bash -e
 
 BASE_URL=http://localhost:8080
+LOGIN_URL=http://localhost:3000
 TOKEN="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjc5NTUxODc3NDJ9.l5PJbjxZJnbt1wI3qRMlMgkJVRKxeTokD3K_AM6ZxCs"
 
 # Start the application
 docker compose down
-docker compose up -d --build
-while ! curl -f $BASE_URL/; do sleep 1; done
+docker compose up -d
+while ! curl -f $BASE_URL/health; do sleep 1; done
 
 # / redirect to login page
-res=$(curl $BASE_URL)
+res=$(curl -L $BASE_URL)
 if [[ !("$res" =~ "<title>Login</title>") ]]; then
   echo "Test failed: / redirect to login page"
   echo $res
   exit 1
 fi
 
-# login
-status=$(curl -X POST $BASE_URL/login --data-urlencode "password=1234&redirect_to=$BASE_URL" -w "%{http_code}")
-if [[ $status != 303 ]]; then
-  echo "Test failed: login"
-  echo "status=$status (expected 303)"
+# login redirect to /
+status=$(curl -X POST $BASE_URL -H "Content-Type: application/x-www-form-urlencoded" -H "Host: login" --data-urlencode "password=1234&redirect_to=$BASE_URL" -w "%{http_code}")
+if [ $status != 303 ]; then
+  echo "Test failed: login redirect to /"
+  echo $status
   exit 1
 fi
 
